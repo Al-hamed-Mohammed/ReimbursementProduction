2ced8d3cb915542dec8abf34b70523bf48495fb7
Awez <awez.vtt@gmail.com>
2021-06-12 03:30:57 +0530

    blob storage image upload, view and delete are done

---
+++ EmployeeManager2/Data/BlobConnectionClass.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManager2.Data
+{
+    public static class BlobConnectionClass
+    {
+        static string account = "reimbursmentstorage";
+        static string key = "taOqpnL5gGFkrFES3cBQOlaH5ThJv/SGt8mEZDlNMFHRaCIAHkreI970FImjloDExfO2dcpeyf3v/SRHbkNGtg==";             
+        public static CloudStorageAccount GetConnectionString()
+        {
+            string connectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", account, key);
+            return CloudStorageAccount.Parse(connectionString);
+        }
+    }
+}
