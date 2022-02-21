9165ceea0dd3fd2623f0f37542afb769b6ab3503
Ansar <ansarvaseem2016@gmail.com>
2022-02-22 04:03:37 +0530

    Adding ClientNumber Col
    Adding ClientNumber Col

--- EmployeeManager2/Migrations/20220218183308_addcol.Designer.cs
+++ EmployeeManager2/Migrations/20220218183308_addcol.Designer.cs
@@ -4,14 +4,16 @@ using EmployeeManager2.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EmployeeManager2.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220218183308_addcol")]
+    partial class addcol
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmployeeManager2/Migrations/20220218183308_addcol.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManager2.Migrations
+{
+    public partial class addcol : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClientNumber",
+                table: "Employee",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientNumber",
+                table: "Employee");
+        }
+    }
+}
